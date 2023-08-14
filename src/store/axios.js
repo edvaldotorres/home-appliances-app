3f72ba0029a271aab55b25264a18cf1e1c45710d
edvaldotorres <edvaldo.torres@tisaude.com>
2023-08-14 16:01:14 -0300

    refactor: refactor axios

--- src/store/axios.js
+++ src/store/axios.js
@@ -1,21 +1,15 @@
-import axios from "axios";
+import { $http } from "../http";
 
 // Add a request interceptor
-axios.interceptors.request.use(
-    config => {
-        const token = 'localStorageService.getAccessToken()';
-        if (token) {
-            config.headers['Authorization'] = `Bearer ${process.env.VUE_APP_API_KEY}`;
-        }
-        return config;
-    },
-    error => {
-        Promise.reject(error)
-    });
-
-//Add a response interceptor
-axios.interceptors.response.use((response) => {
-    return response
-}, function(error) {
-    return Promise.reject(error);
-});
+$http.interceptors.request.use(
+  (config) => {
+    const token = "localStorageService.getAccessToken()";
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${process.env.VUE_APP_API_KEY}`;
+    }
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+);
